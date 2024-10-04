89347c848098e698e64fd0f707738f6a7c1dc0aa
VAbrishamii <vahide.abri@yahoo.com>
2024-10-04 21:27:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   appType: "mpa",
-  base: "/vasa-social-media/",
+  base: "/",
   build: {
-    outDir: "docs",
+    outDir: "dist",
     target: "esnext",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./vasa-social-media/index.html"),
+        main: resolve(__dirname, "./index.html"),
         login: resolve(__dirname, "./auth/login/index.html"),
         auth: resolve(__dirname, "./auth/index.html"),
-        register: resolve(__dirname, "./vasa-social-media/auth/register/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
         profile: resolve(__dirname, "./profile/index.html"),
         post: resolve(__dirname, "./post/index.html"),
         editPost: resolve(__dirname, "./post/edit/index.html"),
